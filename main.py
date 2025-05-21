403d5b2cc36b31108c5cb8fc3cb1c65b18bedd7c
Wiktor Kycia <wiktor@debian_lepszy.pl>
2025-05-21 22:47:12 +0200

    create a class for displaying the menu of buttons

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+class Canvas(tk.Canvas):
+    def __init__(self, parent, controller):
+        tk.Canvas.__init__(self, parent)
+
+
+class ShapeMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        circles_button = tk.Button(
+            self,
+            text="Circles",
+            command=controller.draw_circles
+        )
+        
+        rectangles_button = tk.Button(
+            self,
+            text="Rectangles",
+            command=controller.draw_rectangles
+        )
+
+        
 class Window(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         # Adding a title to the window
         self.wm_title("Drawing Application")
+        self.geometry("600x500")
         
-        # creating a frame and assigning it to container
-        container = tk.Frame(self, height=400, width=600)
-        # specifying the region where the frame is packed in root
-        container.pack(side="top", fill="both", expand=True)
+        shape_menu = ShapeMenu(self, self)
+        shape_menu.pack(side="top")
         
-        # configuring the location of the container using grid
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+    def draw_circles():
+        pass
+    
+    def draw_rectangles():
+        pass
 
 if __name__ == "__main__":
     testObj = Window()
