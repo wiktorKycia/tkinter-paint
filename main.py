d14867eee3ac0c8412da8cb524dd294e78a4eb2d
Wiktor Kycia <wiktor@debian_lepszy.pl>
2025-05-21 22:27:57 +0200

    add a basic class for displaying window

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk
+
+class Window(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        # Adding a title to the window
+        self.wm_title("Drawing Application")
+        
