d44bd67c0664e07595a70d7e92afdb9585edc20b
Wiktor Kycia <wiktor@debian_lepszy.pl>
2025-05-21 22:04:23 +0200

    made some changes in order to run on other machine

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-from flask import Flask
-import random
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return f"<h1>Hello world! {random.randint(1, 10)}</h1>"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+import sys
+print(sys.version)
